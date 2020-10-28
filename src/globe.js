3132e74e9297c9a6212e9a50c20fded46b1e6a1d
matbabs <matbabs78@gmail.com>
2020-10-28 23:50:19 +0100

    fx

--- src/globe.js
+++ src/globe.js
@@ -11,6 +11,9 @@ var numPlanes = document.getElementById("numPlanes")
 var planesMarker = []
 var xhr_planes = new XMLHttpRequest();
 
+var vessels;
+var xhr_vessels = new XMLHttpRequest();
+
 listenXMLhttpRequest();
 
 /* GLOBE ====================================================================== */
@@ -31,9 +34,11 @@ function initGlobe() {
         document.getElementById('coords').innerHTML = e.latlng.lat + ', ' + e.latlng.lng;
     });*/
 
-    rotate()
 
     getPlanes()
+    getVessels()
+
+    rotate()
 }
 
 function rotate(){
@@ -68,7 +73,7 @@ function drawPlanes(){
     let i = 0
     planes.states.forEach( plane => {
         if(numPlanes.value != 0 && i%(11-numPlanes.value)==0){
-            let m = WE.marker([plane[6], plane[5]],'../assets/_plane1.png', 32, 45).addTo(map);
+            let m = WE.marker([plane[6], plane[5]],'../assets/_plane.png', 32, 32).addTo(map);
             m.bindPopup(
                 "<b>icao24:</b> "+plane[0]+"<br>"+
                 "<b>callsign:</b> "+plane[1]+"<br>"+
@@ -83,11 +88,41 @@ function drawPlanes(){
     });
 }
 
+
+function drawVessels(){
+    /*planesMarker.forEach(m => m.removeFrom(map))
+    if(numPlanes.value < 0)
+        numPlanes.value = 0
+    if(numPlanes.value > 10)
+        numPlanes.value = 10*/
+    let i = 0
+    vessels.features.forEach( vessel => {
+        if(i%10==0){
+            let m = WE.marker([vessel.geometry.coordinates[1], vessel.geometry.coordinates[0]],'../assets/_boat.png', 32, 32).addTo(map);
+            /*m.bindPopup(
+                "<b>icao24:</b> "+plane[0]+"<br>"+
+                "<b>callsign:</b> "+plane[1]+"<br>"+
+                "<b>origin country:</b> "+plane[2]+"<br>"+
+                "<b>longitude:</b> "+plane[5]+"<br>"+
+                "<b>latitude:</b> "+plane[6]+"<br>"+
+                "<b>baro altitude:</b> "+plane[7]+"<br>"
+                ,{maxWidth: 150})
+            planesMarker.push(m)*/
+        }
+        i++;
+    });
+}
+
 /* HTTP ====================================================================== */
 
 function getPlanes(){
     xhr_planes.open('GET','https://opensky-network.org/api/states/all')
     xhr_planes.send()
+}
+
+function getVessels(){
+    xhr_vessels.open('GET','https://meri.digitraffic.fi/api/v1/locations/latest')
+    xhr_vessels.send()
 }
 
 function listenXMLhttpRequest(){
@@ -96,6 +131,13 @@ function listenXMLhttpRequest(){
         if (xhr_planes.readyState == 4 && xhr_planes.status == 200){
             planes =  JSON.parse(xhr_planes.responseText)
             drawPlanes()
+        }
+    }
+
+    xhr_vessels.onreadystatechange=(e)=>{
+        if (xhr_vessels.readyState == 4 && xhr_vessels.status == 200){
+            vessels =  JSON.parse(xhr_vessels.responseText)
+            drawVessels()
         }
     }
 
