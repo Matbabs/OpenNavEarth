1a97e9ee7c0725dc6be1d3b403c928c98b5e49bd
matbabs <matbabs78@gmail.com>
2020-10-29 11:53:21 +0100

    satellite

--- src/globe.js
+++ src/globe.js
@@ -14,11 +14,26 @@ var xhr_planes = new XMLHttpRequest();
 var vessels;
 var xhr_vessels = new XMLHttpRequest();
 
-listenXMLhttpRequest();
+var iss;
+var xhr_iss = new XMLHttpRequest();
+var issCrew;
+var issCrewHTML = "";
+var xhr_issCrew = new XMLHttpRequest();
+var issMarker;
+var numISS = document.getElementById("numISS")
+
+var satellites;
+var xhr_satellites = new XMLHttpRequest();
+var satellitesView = 1
+var numSatellites = document.getElementById("numSatellites")
+var satellitesMarker = []
 
 /* GLOBE ====================================================================== */
 
 function initGlobe() {
+
+    listenXMLhttpRequest();
+
     map = WE.map('map', {
         center: [36.057944835, -112.18688965],
         zoom: 2,
@@ -34,9 +49,10 @@ function initGlobe() {
         document.getElementById('coords').innerHTML = e.latlng.lat + ', ' + e.latlng.lng;
     });*/
 
-
     getPlanes()
-    getVessels()
+    //getVessels()
+    getISS()
+    getSattellites(satellitesView)
 
     rotate()
 }
@@ -53,7 +69,7 @@ function rotate(){
             requestAnimationFrame(animate);
         }
     });
-    hasToRotate ?  playButton.innerText = "pause_circle_outline" :  playButton.innerText = "play_circle_outline"
+    hasToRotate ? playButton.innerText = "pause_circle_outline" :  playButton.innerText = "play_circle_outline"
 }
 
 function setRotateSpeed(){
@@ -113,6 +129,59 @@ function drawVessels(){
     });
 }
 
+function drawISSCrew(){
+    issCrewHTML += "<br><b>crew:</b> "+issCrew.people.length+"<br>"
+    issCrew.people.forEach( p => {
+        if(p.craft === "ISS"){
+            issCrewHTML += p.name + "<br>"
+        }
+    })
+    drawISS()
+}
+
+function drawISS(){
+    if(issMarker){
+        issMarker.removeFrom(map)
+        issMarker = null
+    }
+    if(iss && numISS.checked){
+        issMarker = WE.marker([iss.iss_position.latitude,iss.iss_position.longitude],'../assets/_iss.png', 32, 32).addTo(map);
+        issMarker.bindPopup(
+            "<b>satellite:</b> ISS<br>"+
+            "<b>latitude:</b> "+iss.iss_position.latitude+"<br>"+
+            "<b>longitude:</b> "+iss.iss_position.longitude+"<br>"+
+            "<b>speed:</b> 28000km/h<br>"+
+            "<b>baro altitude:</b> 325km<br>"+
+            issCrewHTML
+            ,{maxWidth: 150})
+    }
+}
+
+function drawSatellites(){
+    satellitesMarker.forEach(m => m.removeFrom(map))
+    if(numSatellites.value < 0)
+        numSatellites.value = 0
+    if(numSatellites.value > 10)
+        numSatellites.value = 10
+    let i = 0
+    satellites.above.forEach( satellite => {
+        if(numSatellites.value != 0 && i%(11-numSatellites.value)==0){
+            let m = WE.marker([satellite.satlat, satellite.satlng],'../assets/_satellite.png', 32, 32).addTo(map);
+            m.bindPopup(
+                "<b>satellite id:</b> "+satellite.satid+"<br>"+
+                "<b>satellite name:</b> "+satellite.satname+"<br>"+
+                "<b>int designator:</b> "+satellite.intDesignator+"<br>"+
+                "<b>launchDate:</b> "+satellite.launchDate+"<br>"+
+                "<b>latitude:</b> "+satellite.satlat+"<br>"+
+                "<b>longitude:</b> "+satellite.satlng+"<br>"+
+                "<b>altitude:</b> "+satellite.satalt+"<br>"
+                ,{maxWidth: 150})
+            satellitesMarker.push(m)
+        }
+        i++;
+    });
+}
+
 /* HTTP ====================================================================== */
 
 function getPlanes(){
@@ -125,12 +194,42 @@ function getVessels(){
     xhr_vessels.send()
 }
 
+function getISS(){
+    if(!issCrew){
+        xhr_issCrew.open('GET','http://api.open-notify.org/astros.json')
+        xhr_issCrew.send()
+    }
+    xhr_iss.open('GET','http://api.open-notify.org/iss-now.json')
+    xhr_iss.send()
+}
+
+function getSattellites(view){
+    switch (view) {
+        case 1:
+            //NewYork
+            xhr_satellites.open('GET','https://cors-anywhere.herokuapp.com/https://api.n2yo.com/rest/v1/satellite/above/40.712/-74.006/0/90/0/&apiKey=GVV2RA-H4Z2WA-YL7F7J-4L10')
+            xhr_satellites.send()
+            break;
+        case 2:
+            //Taiwan
+            xhr_satellites.open('GET','https://cors-anywhere.herokuapp.com/https://api.n2yo.com/rest/v1/satellite/above/23.973/120.982/0/90/0/&apiKey=GVV2RA-H4Z2WA-YL7F7J-4L10')
+            xhr_satellites.send()
+            break;
+        case 3:
+            //Paris
+            xhr_satellites.open('GET','https://cors-anywhere.herokuapp.com/https://api.n2yo.com/rest/v1/satellite/above/48.856/2.351/0/90/0/&apiKey=GVV2RA-H4Z2WA-YL7F7J-4L10')
+            xhr_satellites.send()
+            break;
+    }  
+}
+
 function listenXMLhttpRequest(){
 
     xhr_planes.onreadystatechange=(e)=>{
         if (xhr_planes.readyState == 4 && xhr_planes.status == 200){
             planes =  JSON.parse(xhr_planes.responseText)
             drawPlanes()
+            setTimeout(getPlanes,180000)
         }
     }
 
@@ -141,4 +240,37 @@ function listenXMLhttpRequest(){
         }
     }
 
+    xhr_iss.onreadystatechange=(e)=>{
+        if (xhr_iss.readyState == 4 && xhr_iss.status == 200){
+            iss =  JSON.parse(xhr_iss.responseText)
+            drawISS()
+            setTimeout(getISS,10000)
+        }
+    }
+
+    xhr_issCrew.onreadystatechange=(e)=>{
+        if (xhr_issCrew.readyState == 4 && xhr_issCrew.status == 200){
+            issCrew = JSON.parse(xhr_issCrew.responseText)
+            drawISSCrew()
+        }
+    }
+
+    xhr_satellites.onreadystatechange=(e)=>{
+        if (xhr_satellites.readyState == 4 && xhr_satellites.status == 200){
+            satellitesView+=1
+            if(!satellites){
+                satellites = JSON.parse(xhr_satellites.responseText)
+            }else{
+                satellites.above = satellites.above.concat(JSON.parse(xhr_satellites.responseText).above)
+            }
+            getSattellites(satellitesView)
+            drawSatellites()
+            if(satellitesView == 3){
+                setTimeout(() => {
+                    satellitesView = 1
+                    getPlanes(satellitesView)
+                },180000)
+            }
+        }
+    }
 }
